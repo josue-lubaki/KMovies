114b24a3df3511ce21355ed15560a91fac3763e0
Josue Lubaki <josue.lubaki@alithya.com>
2023-05-02 16:45:48 -0400

    added Detail Feature

--- androidApp/src/main/java/ca/josue_lubaki/kmovies/android/di/AppModule.kt
+++ androidApp/src/main/java/ca/josue_lubaki/kmovies/android/di/AppModule.kt
@@ -1,9 +1,14 @@
 package ca.josue_lubaki.kmovies.android.di
 
+import ca.josue_lubaki.kmovies.android.detail.DetailViewModel
 import ca.josue_lubaki.kmovies.android.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     viewModel { HomeViewModel(get()) }
+    viewModel { params -> DetailViewModel(
+        getMovieUseCase = get(),
+        movieId = params.get()
+    ) }
 }
